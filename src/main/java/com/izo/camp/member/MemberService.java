c6bf3cb8c1df509b40f93cbd483961987c62b8a1
ddHyun <ddHyun@github.com>
2022-06-26 11:10:21 +0900

    docs: memberCheck.jsp

--- src/main/java/com/izo/camp/member/MemberService.java
+++ src/main/java/com/izo/camp/member/MemberService.java
@@ -27,11 +27,12 @@ public class MemberService {
 	
 //	컨트롤러 -> 서비스 : 메서드명(변수)의 변수는 컨트롤러에서 보낸 타입과 일치
 //  매퍼 -> 서비스  : 리턴과 메서드 타입 일치
-	//이름, 생일 일치 idx
+	//이름, 생일 일치하는 idx
 	public int getMemberIdx(MemberVO vo){
 		return memberMapper.memberIdx(vo);
 	}
 	
+	//아이디, 비번 일치하는 idx
 	public int getIdxFromId(MemberVO vo) {
 		return memberMapper.memberIdxFromId(vo);
 	}
