90a68ae61e7b8756d6e7f1ea9dbb2eb5f46de13b
lyrWinterCat <103623292+lyrWinterCat@users.noreply.github.com>
2022-07-04 16:07:44 +0900

    docs: sql

--- src/main/resources/datafile/FinalProducts.sql
+++ src/main/resources/datafile/FinalProducts.sql
@@ -1,17 +1,18 @@
 /*상품 1 */
 INSERT INTO TEST_PM.PRODUCTTABLE
 (IDX, PRODUCTID, PRICE, STOCK, IMGNAME)
-VALUES(SEQ_PRODUCT.NEXTVAL, '부대찌개', 13000, 300, 'foodNum01');
+VALUES(SEQ_PRODUCT.NEXTVAL, '바베큐폭립', 40500, 3, 'foodNum01');
 /*================================상품 디테일 ============================*/
 INSERT INTO TEST_PM.P_DETAILTABLE
 (IDX, PRODUCTID, CONTENT, WEIGHT, DETAILIMG1, DETAILIMG2, DETAILIMG3, DETAILIMG4, DETAILIMG5)
-VALUES(SEQ_P_Detail, '부대찌개', '', '', '', '', '', '', '');
+VALUES(SEQ_P_Detail, '바베큐폭립', '독일산 참나무로 훈연한 부드러운 등갈비와 특제 바베큐 소스의 향연', '450g', 
+'rib1.jpg', 'rib2.jpg', 'rib3.jpg', 'rib4.jpg', 'rib5.jpg');
 
 
 /*상품 2 */
 INSERT INTO TEST_PM.PRODUCTTABLE
 (IDX, PRODUCTID, PRICE, STOCK, IMGNAME)
-VALUES(SEQ_PRODUCT.NEXTVAL, '삼겹살', 15000, 200, 'foodNum02');
+VALUES(SEQ_PRODUCT.NEXTVAL, '삼겹살', 11900, 3, 'foodNum02');
 /*================================상품 디테일 ============================*/
 INSERT INTO TEST_PM.P_DETAILTABLE
 (IDX, PRODUCTID, CONTENT, WEIGHT, DETAILIMG1, DETAILIMG2, DETAILIMG3, DETAILIMG4, DETAILIMG5)
@@ -21,11 +22,13 @@ VALUES(SEQ_P_Detail, '삼겹살', '', '', '', '', '', '', '');
 /*상품 3 */
 INSERT INTO TEST_PM.PRODUCTTABLE
 (IDX, PRODUCTID, PRICE, STOCK, IMGNAME)
-VALUES(SEQ_PRODUCT.NEXTVAL, '오삼불고기', 13000, 200, 'foodNum03');
+VALUES(SEQ_PRODUCT.NEXTVAL, '감바스 알 아히요 2인분/쿠킹박스', 11900, 3, 'foodNum03');
 /*================================상품 디테일 ============================*/
 INSERT INTO TEST_PM.P_DETAILTABLE
 (IDX, PRODUCTID, CONTENT, WEIGHT, DETAILIMG1, DETAILIMG2, DETAILIMG3, DETAILIMG4, DETAILIMG5)
-VALUES(SEQ_P_Detail, '오삼불고기', '', '', '', '', '', '', '');
+VALUES(SEQ_P_Detail, '감바스 알 아히요 2인분/쿠킹박스', '스페인 분위기 그대로 담은 감바스는 올리브 오일에 마늘과 베트남 고추의 향을 입혀 
+새우를 넣고 끓여 담백하면서 풍성한 맛이 일품', '2인분', 
+'shrimp1.jpg', 'shrimp2.jpg', 'shrimp3.jpg', 'shrimp4.jpg', 'shrimp5.jpg');
 
 
 /*상품 4 */
@@ -42,11 +45,13 @@ VALUES(SEQ_P_Detail, '사천마라탕', '', '', '', '', '', '', '');
 /*상품 5 */
 INSERT INTO TEST_PM.PRODUCTTABLE
 (IDX, PRODUCTID, PRICE, STOCK, IMGNAME)
-VALUES(SEQ_PRODUCT.NEXTVAL, '우삼겹 된장찌개', 12000, 300, 'foodNum05');
+VALUES(SEQ_PRODUCT.NEXTVAL, '스테이크 플래터(냉동)',35900, 3, 'foodNum05');
 /*================================상품 디테일 ============================*/
 INSERT INTO TEST_PM.P_DETAILTABLE
 (IDX, PRODUCTID, CONTENT, WEIGHT, DETAILIMG1, DETAILIMG2, DETAILIMG3, DETAILIMG4, DETAILIMG5)
-VALUES(SEQ_P_Detail, '우삼겹 된장찌개', '', '', '', '', '', '', '');
+VALUES(SEQ_P_Detail, '스테이크 플래터(냉동)', '육즙이 풍부한 양갈비와 부채살이 쫄깃한 식감과 고소한 감칠맛을 선사하고
+그린빈, 감자 등으로 구성된 풍성한 가니쉬도 곁들여 캠핑장에서도 근사한 한 끼 식사를 완성해보세요!', '1068g', 
+'steak1.jpg', 'steak2.jpg', 'steak3.jpg', 'steak4.jpg', 'steak5.jpg');
 
 
 /*상품 6 */
@@ -62,11 +67,13 @@ VALUES(SEQ_P_Detail, '스테이크 플래터', '', '', '', '', '', '', '');
 /*상품 7 */
 INSERT INTO TEST_PM.PRODUCTTABLE
 (IDX, PRODUCTID, PRICE, STOCK, IMGNAME)
-VALUES(SEQ_PRODUCT.NEXTVAL, '밀푀유나베', 15000, 200, 'foodNum07');
+VALUES(SEQ_PRODUCT.NEXTVAL, '토마호크스테이크', 75900, 3, 'foodNum07');
 /*================================상품 디테일 ============================*/
 INSERT INTO TEST_PM.P_DETAILTABLE
 (IDX, PRODUCTID, CONTENT, WEIGHT, DETAILIMG1, DETAILIMG2, DETAILIMG3, DETAILIMG4, DETAILIMG5)
-VALUES(SEQ_P_Detail, '밀푀유나베', '', '', '', '', '', '', '');
+VALUES(SEQ_P_Detail, '토마호크스테이크', '소갈비뼈가 통째로 붙어있어 러스틱한 느낌 그대로의 뉴욕 정통 스테이크 스타일, 자유롭고 다채로운 식문화로 
+미식의 천국인 뉴욕의 감각적인 맛과 멋을 만끽해 보세요!', '940g', 
+'toma1.jpg', 'toma2.jpg', 'toma3.jpg', 'toma4.jpg', 'toma5.jpg');
 
 
 
@@ -83,11 +90,13 @@ VALUES(SEQ_P_Detail, '감바스 알아히요', '', '', '', '', '', '', '');
 /*상품 9 */
 INSERT INTO TEST_PM.PRODUCTTABLE
 (IDX, PRODUCTID, PRICE, STOCK, IMGNAME)
-VALUES(SEQ_PRODUCT.NEXTVAL, '만두전골', 12000, 200, 'foodNum09');
+VALUES(SEQ_PRODUCT.NEXTVAL, '캠핑음식 꼬치구이 세트', 12900, 3, 'foodNum09');
 /*================================상품 디테일 ============================*/
 INSERT INTO TEST_PM.P_DETAILTABLE
 (IDX, PRODUCTID, CONTENT, WEIGHT, DETAILIMG1, DETAILIMG2, DETAILIMG3, DETAILIMG4, DETAILIMG5)
-VALUES(SEQ_P_Detail, '만두전골', '', '', '', '', '', '', '');
+VALUES(SEQ_P_Detail, '캠핑음식 꼬치구이 세트', '캠핑장에서 즐기는 아는 사람들은 다 안다는 그 맛, 이번 캠핑은
+꼬치세트와 함께 즐겨보세요!', '모듬꼬치 10개', 
+'chicken1.jpg', 'chicken2.jpg', 'chicken3.jpg', 'chicken4.jpg', 'chicken5.jpg');
 
 
 
